f5573d27f72be4a14d25818c0a920b4ad6c93a22
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2023-12-25 02:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let num2 = 20;
 
 	for(let i = num1; i<=num2.length; i++){
 		if(arr[i]%3 == 0){
-			console.log("Fizz");
+			output="Fizz";
 		}
 		else if(arr[i]%5 == 0){
-			console.log("Buzz");
+			output="Buzz";
 		}
 		else if(arr[i]%3 == 0 && arr[i]%5 == 0){
-			console.log("FizzBuzz");
+			output="FizzBuzz";
 		}
 	}
-
-
+let result = output;
+console.log(result);
